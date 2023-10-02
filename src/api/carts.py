3daa83ddc83fb76e91e15e7aa9058a748a8d4f1d
Sebi2k <96271751+Sebi2k@users.noreply.github.com>
2023-10-02 11:40:05 -0700

    worked on carts

--- src/api/carts.py
+++ src/api/carts.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends, Request
 from pydantic import BaseModel
 from src.api import auth
-# import sqlalchemy
-# from src import database as db
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
     prefix="/carts",
@@ -10,6 +10,8 @@ router = APIRouter(
     dependencies=[Depends(auth.get_api_key)],
 )
 
+carts = {}
+id = 0
 
 class NewCart(BaseModel):
     customer: str
@@ -18,7 +20,13 @@ class NewCart(BaseModel):
 @router.post("/")
 def create_cart(new_cart: NewCart):
     """ """
-    return {"cart_id": 1}
+    global id
+    global carts
+
+    id += 1
+    carts[id] = {}
+
+    return {"cart_id": id}
 
 
 @router.get("/{cart_id}")
@@ -35,7 +43,8 @@ class CartItem(BaseModel):
 @router.post("/{cart_id}/items/{item_sku}")
 def set_item_quantity(cart_id: int, item_sku: str, cart_item: CartItem):
     """ """
-
+    global carts
+    carts[id][item_sku] = cart_item.quantity
     return "OK"
 
 
@@ -45,5 +54,12 @@ class CartCheckout(BaseModel):
 @router.post("/{cart_id}/checkout")
 def checkout(cart_id: int, cart_checkout: CartCheckout):
     """ """
+
+    with db.engine.begin() as connection:
+        sql = "UPDATE global_inventory SET gold = gold + 50"
+        connection.execute(sqlalchemy.text(sql))
+
+        sql2 = "UPDATE global_inventory SET num_red_potions = num_red_potions - 1"
+        connection.execute(sqlalchemy.text(sql2))
 
     return {"total_potions_bought": 1, "total_gold_paid": 50}
