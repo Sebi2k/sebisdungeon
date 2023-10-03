25c54220afc1fe6e628da639c69f99592c999c1e
Sebi2k <96271751+Sebi2k@users.noreply.github.com>
2023-10-02 21:10:58 -0700

    database.py potential fix

--- src/database.py
+++ src/database.py
@@ -5,6 +5,6 @@ from sqlalchemy import create_engine
 def database_connection_url():
     dotenv.load_dotenv()
 
-    return os.environ.get("postgresql://postgres:sebisdungeon123@db.tpydkxcvvrqbrruhzoaf.supabase.co:5432/postgres")
+    return os.environ.get("POSTGRES_URI")
 
 engine = create_engine(database_connection_url(), pool_pre_ping=True)
